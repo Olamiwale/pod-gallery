3482775fde8c162ed0566b4f45dc53a8f944ac67
olamiwale <olamiwalepaul@gmail.com>
2026-09-03 22:26:14 +0100

    update

--- app/data/artwork.ts
+++ app/data/artwork.ts
@@ -13,7 +13,7 @@ export const artworks: Artwork[] = [
 		title: "Golden Silence",
 		size: "90 × 120 cm",
 		price: "₦650,000",
-		image: "/art1.png",
+		image: "/artwork1.png",
 	},
 
 	{
@@ -22,14 +22,14 @@ export const artworks: Artwork[] = [
 		size: "70 × 100 cm",
 		price: "₦600,000",
 		image:
-			"/art8.png",
+			"/artwork2.png",
 	},
 
 	{
 		title: "Golden Silence",
 		size: "90 × 120 cm",
 		price: "₦650,000",
-		image: "/art2.png",
+		image: "/artwork3.png",
 	},
 	
 	{
@@ -37,24 +37,10 @@ export const artworks: Artwork[] = [
 	
 		size: "100 × 150 cm",
 		price: "₦820,000",
-		image:"/art3.png",
+		image:"/artwork4.png",
 	},
-	{
-		title: "Beyond the Horizon",
 	
-		size: "80 × 100 cm",
-		price: "₦550,000",
-		image:
-		"/art4.png",
-	},
-	{
-		title: "Balance in Chaos",
-	
-		size: "100 × 120 cm",
-		price: "₦900,000",
-		image:
-			"/art5.png",
-	},
+
 
 	{
 		title: "Golden Silence",
@@ -88,14 +74,7 @@ export const artworks: Artwork[] = [
 		image:
 			"https://images.unsplash.com/photo-1541961017774-22349e4a1262?auto=format&fit=crop&w=1200&q=90",
 	},
-	{
-		title: "Echoes Within",
-		
-		size: "100 × 120 cm",
-		price: "₦750,000",
-		image:
-			"/art6.png",
-	},
+
 	{
 		title: "Echoes Within",
 	
@@ -113,22 +92,7 @@ export const artworks: Artwork[] = [
 			"https://images.unsplash.com/photo-1578301978018-3005759f48f7?auto=format&fit=crop&w=1200&q=90",
 	},
 	
-	{
-		title: "Unseen Strength",
-	
-		size: "70 × 100 cm",
-		price: "₦600,000",
-		image:
-			"/art7.png",
-	},
-	{
-		title: "Unseen Strength",
-	
-		size: "70 × 100 cm",
-		price: "₦600,000",
-		image:
-			"/art8.png",
-	},
+
 
 
 ];
